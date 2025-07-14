3fbcbb9b4434053cd2e766cc2dc818de734e6303
Jhossett <45399284+jhossettstalinn@users.noreply.github.com>
2025-07-14 14:37:40 -0300

    Create triangle_quest_2.py

---
+++ python/math/triangle_quest_2.py
@@ -0,0 +1,3 @@
+
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((round(10**(i-1)+10**(i-2)+10**(i-3)+10**(i-4)+10**(i-5)+10**(i-6)+10**(i-7)+10**(i-8)+10**(i-9))**2))
